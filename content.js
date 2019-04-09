83b9e7044f4d990f2a9be8b5f7b4c39e0cc44b56
SM Raiyyan <raiyyan.sm@gmail.com>
2019-04-09 12:24:23 +0530

    Initial Commit

--- content.js
+++ content.js
@@ -1,8 +1,11 @@
 $(document).ready(function(){
 	var prevCount = 0;
 	var prevScrollPos = 0;
+	
+	// function to display the upvotes
 	function showUpvotes()
 	{
+	    // getting the elements from web page
 	    var li = document.getElementsByClassName("count hide_in_feed");
 	    var newCount = li.length;
 	    if(newCount != prevCount){
@@ -13,6 +16,8 @@ $(document).ready(function(){
 	    }
 	}
 	showUpvotes();
+	
+	// invoking the function if scrolled
 	document.onscroll = function(){
 		if($(document).scrollTop() >= prevScrollPos + 700){
 			showUpvotes();
